a72da25a758d998189a3c1ca4e869fec5b1c2ac8
Francis <francisjkavanagh@gmail.com>
2025-11-25 13:37:54 +0000

    Cleaned up code and added comments
    Cleaned up code to make it easier to read and added comments on lines missing comments before creating new methods

--- src/main/java/com/mycompany/bankatmmachine/BankATMmachine.java
+++ src/main/java/com/mycompany/bankatmmachine/BankATMmachine.java
@@ -4,58 +4,61 @@ import java.util.Scanner;
 
 public class BankATMmachine {
 
-    static Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);  // create "input" scanner for user input
 
     public static void main(String[] args) {
-        String[] usernames = {"Theo", "Mira", "Jasper", "John", "Amy", "Anna",};
-        String[] passwords = {"pass1", "pass2", "pass3", "pass4", "pass4", "pass5"};
-        double[] balances = {1200.50, 25070.80, 5060.70, 15000.10, 7050.15, 2970.30};
-        int index = LogIn(usernames, passwords);
-        menu(usernames, passwords, balances, index);
+        String[] usernames = {"Theo", "Mira", "Jasper", "John", "Amy", "Anna",}; // Array for valid username
+        String[] passwords = {"pass1", "pass2", "pass3", "pass4", "pass4", "pass5"}; // Array for valid passwords
+        double[] balances = {1200.50, 25070.80, 5060.70, 15000.10, 7050.15, 2970.30}; // Array for balances matching user & pass indexes
+        boolean[] overdraft = {false, false, false, false, false}; // all users start with false overdraft
+        
+        int index = LogIn(usernames, passwords); // when login is successful, the index of username as password arrays is assigned to variable "index", this makes finding matching balance, etc easier
+        
+        menu(usernames, passwords, balances, index); // run menu method after successful login
     }
 
-    static int LogIn(String[] usernames, String[] passwords) {
-        String name = null;// to compare user input to the usernames array and find the name
-        String pass = null;// to compare user input to the password that is assigned to the user
-        boolean validLogin = false;
-        boolean userFound = false;
-        int loggedUserIndex = -1;
-        do {//while loop to get persons username
-            System.out.println("Enter your username");
-            name = input.nextLine();//input to compare users input to the array
-            for (int i = 0; i < usernames.length; i++) {//loop through array to find username
+    static int LogIn(String[] usernames, String[] passwords) { // method to check for successful login
+        String name = null; // to compare user input to the usernames array and find the name
+        String pass = null; // to compare user input to the password that is assigned to the user
+        boolean validLogin = false; // validLogin expected to fail from beginning
+        boolean userFound = false; // userFound expected to fail from beginning
+        int loggedUserIndex = -1; // starts as -1 to avoid invalid accidental login until it is overwritten after successful login
+        do { // while loop to get persons username
+            System.out.print("Enter your username: "); // asks user to enter username
+            name = input.nextLine(); // input to compare users input to the array
+            for (int i = 0; i < usernames.length; i++) { // loop through array to find username
                 if (name.equals(usernames[i])) {
-                    loggedUserIndex = i;//if user found take the index and save it 
-                    userFound = true;//user found 
-                    break;//exit loop
+                    loggedUserIndex = i; // if user found take the index and save it 
+                    userFound = true; // user found 
+                    break; // exit loop
                 }
             }
-            if (!userFound) {//if user not found print message and continue loop
-                System.out.println("User not Found Try Again");
+            if (!userFound) { // if user not found print message and continue loop
+                System.out.println("User not Found Try Again"); // message notifying user username not found
             }
-        } while (userFound == false);//do loop while person enters valid username
+        } while (userFound == false); // do loop while person enters valid username
 
-        int tryMax = 0;//to count the tries and exit when user exceed the 3 tries
-        int tryCount = 3;//to count how many times user tries to guess password and print it out to them
+        int tryMax = 0; // to count the tries and exit when user exceed the 3 tries
+        int tryCount = 3; // to count how many times user tries to guess password and print it out to them
         do {
-            System.out.println("Enter Password");
-            pass = input.nextLine();//input for password to compare it to the original password
+            System.out.println("Enter Password: "); // ask user to enter username
+            pass = input.nextLine(); // input for password to compare it to the original password
             if (pass.equals(passwords[loggedUserIndex])) {// if password matches quit the loop
-                System.out.println("Login was succesful");
-                validLogin = true;
-                break;
+                System.out.println("Login was succesful"); // user notified of successful login
+                validLogin = true; // validLogin switch to true
+                break; // exit loop
             } else {
-                tryMax++;//iff passwords doesnt match increaase the max count
+                tryMax++;//if passwords doesnt match increaase the max count
                 tryCount--;// if password doesnt match decrease tries 
-                System.out.println("Incorrect Password, try again");
-                System.out.println("You have " + tryCount + " tries");//print out to user ow many tries they have left
-                if (tryMax >= 3) {//if user exceeds the try limit program stops
-                    System.out.println("You exceeded the maximum allowed tries. Contact the bank ");
-                    System.exit(0);
+                System.out.println("Incorrect Password, try again"); // user notified incorrect password
+                System.out.println("You have " + tryCount + " tries"); // print out to user ow many tries they have left
+                if (tryMax >= 3) { // if user exceeds the try limit program stops
+                    System.out.println("You exceeded the maximum allowed tries. Contact the bank "); // user notified of maximum login attempt
+                    System.exit(0); // closes entire program after max tries exceeded
                 }
             }
-        } while (validLogin == false && tryMax < 3);
-        return loggedUserIndex;
+        } while (validLogin == false && tryMax < 3); // keep looping while conditions are met
+        return loggedUserIndex; // this returns the index of user after successful login
     }
 
     static void menu(String[] usernames, String[] passwords, double[] balances, int index) {
@@ -73,54 +76,52 @@ public class BankATMmachine {
             choice = input.nextInt();
             input.nextLine();
             switch (choice) {
-                case 1:
-                ///bank statement
+                case 1: //  Francis - Statement Overdraft
                 
-            case 2:
-                /// deposit
+                case 2: // Francis - deposit
                 
-            case 3://withdraw
+                case 3: // Francis - withdraw
 
-                case 4:password(usernames,passwords,index);
+                case 4: // Juste - change password
+                    changePass(usernames,passwords,index);
 
-                case 5:// change username
+                case 5: // Juste - change username
 
-                case 6:
-                    System.out.println("Thank you for using our ATM:");
-                    exit = true;
+                case 6: // Juste - Exit ATM
+                    System.out.println("Thank you for using our ATM:"); // thank user for using ATM
+                    exit = true; // change exit to true
                     
-
-                default:
+                default: // if user input doesn't equal 1-6, print invalid input
                     System.out.println("Invalid input");
             }
-        } while (exit == false);
+        } while (exit == false); // continue looping until exit = true
     }
 
-    static void password(String[] usernames, String[] passwords, int index) {
-        String currPassword = null;
-        String newPassword = null;
-        boolean currpass = false;
-        boolean newpass= false;
+    static void changePass(String[] usernames, String[] passwords, int index) { // method to change password
+        String currPassword = null; // start current password as null to avoid conflicts
+        String newPassword = null; // start new password as null to avoid conflicts
+        boolean currPassMatch = false; // assume password starts as incorrect
+        boolean newPassMatch = false;
         do {
-            System.out.println("Enter Your Current Password");
-            currPassword = input.nextLine();
-            if (currPassword.equals(passwords[index])) {
-                currpass = true;
-                System.out.println("Pass is correct");
-                break;
+            System.out.print("Enter Your Current Password: "); // ask user to enter current password
+            currPassword = input.nextLine(); // assign currPassword to user input
+            if (currPassword.equals(passwords[index])) { // checking if currPassword matches existing password
+                currPassMatch = true; // change booleanto true if password is correct to exit loop
+                System.out.println("Pass is correct"); // display that the password is correct
+                break; // exit loop
             }else{
-                System.out.println("Incorrect Password. Try again");
+                System.out.println("Incorrect Password. Try again"); // if password is incorrect display incorrect password try again and continue the loop
             }
-        } while (currpass == false);
-        while(newpass==false){
-            System.out.println("Enter new password");   
-            newPassword=input.nextLine();
-            if(newPassword==passwords[index]){
-                System.out.println("Passwords cannot be same as the old one");
-                continue;
-            }if(newPassword!=passwords[index]){
-                passwords[index]=newPassword;
-                newpass=true;
+        } while (currPassMatch == false); // do block of code until password is correct and boolean is changed to true
+        while(newPassMatch == false){ // loop for new password o this until person enter valid password and boolean is changed to true
+            System.out.println("Enter new password"); // display to user enter new password
+            newPassword=input.nextLine(); // assign user input to newPassword
+            if(newPassword==passwords[index]){ // compare password to the old one
+                System.out.println("Passwords cannot be same as the old one"); // if password is the same as the old one, display to user password can't be the same as odl one and try again
+                continue; // start loop again
+            }if(newPassword!=passwords[index]){ // if password doesn't match old password, replace old password with new one in array
+                passwords[index]=newPassword; // boolean changed to tre to exit loop
+                newPassMatch = true; // display user the the login was successful
                 
             }
         }
@@ -140,7 +141,4 @@ public class BankATMmachine {
 	users can only withraw more money than they have in their bank account if they have overdraft facility
 ( cannot be the password that was used before)
 ( cannot be more than 20 characters)
-
-// Francis  deposit , withdraw  , statement overdraft
-//Juste *login, *menu, change password,  change username
 */
